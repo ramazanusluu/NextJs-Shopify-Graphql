37e883956dda529ded4d86b9031246b197c4157b
Ramazan USLU <ruslu1720@gmail.com>
2022-07-25 15:38:19 +0300

    Temel kurulum islemleri

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// Hydrogen components are only exported as ESM, not CJS. Shopify will support
+// multiple export types in a future version of Hydrogen, so the extra transpile
+// step is a temporary workaround.
+const withTM = require("next-transpile-modules")(["@shopify/hydrogen"]);
+
+module.exports = withTM({
+  reactStrictMode: true,
+});
