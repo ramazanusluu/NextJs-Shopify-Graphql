ede700baa8c701202045df2d6a867845f3891e72
Ramazan USLU <ruslu1720@gmail.com>
2022-08-03 09:51:17 +0300

    Collection detail sayfasi guncellendi

--- components/CollectionsDetails.js
+++ components/CollectionsDetails.js
@@ -1,18 +1,44 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function CollectionsDetails({ collection }) {
-  console.log("collection", collection);
+  console.log("collection detail", collection);
   return (
-    <div>
-      <h1>Collections Details</h1>
-      {collection.map((item) => {
-        return (
-          <div key={item.node.id}>
-            <h1>{item.node.handle}</h1>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+        <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
+          Products
+        </h2>
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 false  sm:grid-cols-3 false false">
+          {collection.map((item) => {
+            return (
+              <div key={item.node.id}>
+                <Link href={`/product/${item.node.handle}`}>
+                  <a className="group">
+                    <div className="grid gap-4">
+                      <Image
+                        className="rounded shadow-border overflow-clip inline-block aspect-[5/4] md:aspect-[3/2] object-cover"
+                        src={item.node.images.edges[0].node.originalSrc}
+                        width={400}
+                        height={400}
+                        alt={item.node.images.edges[0].node.altText}
+                      />
+                      <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
+                        {item.node.title}
+                      </h2>
+                      <h6 className="text-md font-medium text-gray-600">
+                        {item.node.priceRange.minVariantPrice.amount} ₺
+                      </h6>
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 }
 
